51ed8afca6c1aa0d5213e9177ee80633e11010ff
timitoerner <public.email.basis107@aleeas.com>
2024-07-21 01:59:19 +0200

    added some useful programs

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -129,11 +129,25 @@
   };
 
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    vim
     neovim
     tree
     dust
-  #  wget
+    btop
+    htop
+    wget
+    tlrc
+    neofetch
+    fastfetch
+    zip
+    mpv
+    libqalculate
+    fzf
+    cmatrix
+    feh
+    lolcat
+    cowsay
+    trash-cli
   ];
 
   modules.virt-manager.enable = true;
